9f94777f7ebd00de035f9fa5396650a69ee21f95
Jim Kitchen <jim22k@gmail.com>
2021-01-20 10:31:02 -0600

    Try more Windows options; comment out linux/mac to avoid unnecessary work as I figure this out

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,11 @@
-# make sure CMake install goes in the right place
+setlocal EnableDelayedExpansion
+
+:: make sure CMake install goes in the right place
 SET INSTALL="%PREFIX%"
 SET CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON"
 
-# make SuiteSparse
-make library VERBOSE=1 JOBS=16
-make install VERBOSE=1
+cmake -G "NMake Makefiles"
+
+:: make SuiteSparse
+nmake
+nmake install
