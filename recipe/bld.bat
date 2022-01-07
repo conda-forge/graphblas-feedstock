017e90658852d8b32d5c7c58458022af81c7dd35
Jim Kitchen <jim22k@gmail.com>
2022-01-07 15:21:13 -0600

    Another attempt to get Windows to build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,10 +9,10 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_ARCHIVE_OUTPUT_DIRECTORY=%LIBRARY_LIB% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D BUILD_SHARED_LIBS=ON ^
-      -D GBNCPUFEAT=1 ^
+      -D GBNCPUFEAT=ON ^
       %SRC_DIR%
 if errorlevel 1 exit /b 1
 
-cmake --build . --target install
+cmake -DGBNCPUFEAT=ON --build . --target install
 if errorlevel 1 exit /b 1
 
