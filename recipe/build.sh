5f9583b37be5220636a73793c68a334dac1a9ec0
Erik Welch <erik.n.welch@gmail.com>
2022-01-10 21:54:48 -0600

    [DO NOT MERGE] Try v6.1.4.beta2

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 # make sure CMake install goes in the right place
 export INSTALL="${PREFIX}"
 export CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release"
+if [[ "$OSTYPE" != "linux"* ]]; then
+  export CMAKE_OPTIONS="-DGBNCPUFEAT=1 ${CMAKE_OPTIONS}"
+fi
 
 # make SuiteSparse
 make library VERBOSE=1 JOBS=16
