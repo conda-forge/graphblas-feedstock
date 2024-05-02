f5ec6754e81c27774df7e655781b9812ceb0eb02
Min RK <benjaminrk@gmail.com>
2024-05-02 13:09:32 +0200

    update build to use cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,14 @@
 # make sure CMake install goes in the right place
 export INSTALL="${PREFIX}"
 # Use JITINIT=2 (=run) to use pre-JIT kernels that don't need a compiler at runtime
-export CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DJITINIT=2"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DJITINIT=2"
 if [[ "$OSTYPE" != "linux"* ]]; then
-  export CMAKE_OPTIONS="-DGBNCPUFEAT=1 ${CMAKE_OPTIONS}"
+  export CMAKE_ARGS="${CMAKE_ARGS} -DGBNCPUFEAT=1"
 fi
 
 # Show cmake output on failures
 trap "cat $SRC_DIR/build/CMakeFiles/CMakeOutput.log $SRC_DIR/build/CMakeFiles/CMakeError.log" ERR
 # make SuiteSparse
-make library VERBOSE=1 JOBS=16
-make install VERBOSE=1
-# forcibly remove the static library
-rm -f ${PREFIX}/lib/libgraphblas.a
+cmake -B build ${CMAKE_ARGS} .
+cmake --build build --verbose --parallel "${CPU_COUNT:-1}"
+cmake --install build
