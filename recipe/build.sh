45ac662d1431e72ca57a8361350a6c42e0adcabb
Min RK <benjaminrk@gmail.com>
2024-05-02 13:36:00 +0200

    handle cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-
-# make sure CMake install goes in the right place
-export INSTALL="${PREFIX}"
+set -ex
 # Use JITINIT=2 (=run) to use pre-JIT kernels that don't need a compiler at runtime
-export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DJITINIT=2"
-if [[ "$OSTYPE" != "linux"* ]]; then
+export CMAKE_ARGS="${CMAKE_ARGS} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}\
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DGRAPHBLAS_JITINIT=2 \
+"
+
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CROSSCOMPILING=ON"
+  if [[ "${build_platform}" == "osx-64" ]]; then
+    # seem to need cross toolchain for jit setup
+    # but only on mac-arm
+    export CMAKE_ARGS="${CMAKE_ARGS} -DGRAPHBLAS_CROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_TOOLCHAIN_FILE=/${RECIPE_DIR}/native-toolchain.cmake"
+  fi
+fi
+
+if [[ "${target_platform}" != "linux"* ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DGBNCPUFEAT=1"
 fi
 
